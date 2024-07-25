451a1c9da4ba90ace495111eb8a8fcbb29f1f340
Isaac Rossi <isaacjohnrossi@gmail.com>
2024-07-25 19:12:33 +1000

    Started creating the player search feature: - added custom fonts - customised the tailwind theme - added in dummy player data - created and styled the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+const players = [
+  {
+    name: "Jack Clisby",
+    team: "Western Sydney Wanderers",
+    country: "Australia",
+    role: "defender",
+  },
+  {
+    name: "Keanu Baccus",
+    team: "Western Sydney Wanderers",
+    country: "Australia",
+    role: "midfielder",
+  },
+];
+
 function App() {
   return (
-    <>
-      <p>vstats</p>
-    </>
+    <div className="bg-blue-1000">
+      <div className="container mx-auto">
+        <header className="pt-24 pb-14">
+          <h1 className="mb-12 font-heading text-4xl text-slate-50 uppercase">
+            Players
+          </h1>
+
+          <label htmlFor="search" />
+          <input id="search" type="text" placeholder="Search for a player" />
+        </header>
+
+        <hr />
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1280px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        heading: ["Oswald", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "blue-1000": "#0D1326",
+      },
+    },
   },
   plugins: [],
 };
