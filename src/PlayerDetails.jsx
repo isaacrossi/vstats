1d5f842052ddba099d3191bdfa3058b6bda353b1
Isaac Rossi <isaacjohnrossi@gmail.com>
2024-10-21 17:04:06 +1100

    Fixed some alignment issues within the PlayerDetails header.

--- src/PlayerDetails.jsx
+++ src/PlayerDetails.jsx
@@ -40,87 +40,95 @@ const PlayerDetails = () => {
           <p className="text-slate-50">Loading...</p>
         ) : (
           <>
-            <h2 className="font-heading text-4xl text-slate-50 uppercase mb-1">
-              {player.player.lastname}
-            </h2>
-            <h1 className="font-heading text-6xl text-outline uppercase mb-12">
-              {player.player.firstname}
-            </h1>
-            <div className="flex w-full justify-between">
-              <ul className="">
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">Role</h3>
-                  <p className="text-base uppercase text-slate-50">
-                    {player.statistics[0].games.position}
-                  </p>
-                </li>
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">
-                    Country
-                  </h3>
-                  <div className="flex items-center">
-                    <img
-                      className="h-5 w-auto mr-2"
-                      src={`https://flagsapi.com/${
-                        countries[player.player.nationality]
-                      }/flat/64.png`}
-                      alt={`${player.player.nationality} flag`}
-                    />
-                    <p className="text-base uppercase text-slate-50">
-                      {player.player.nationality}
-                    </p>
-                  </div>
-                </li>
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">Team</h3>
-                  <div className="flex items-center">
-                    <img
-                      className="w-6 h-6 mr-2"
-                      src={player.statistics[0].team.logo}
-                      alt={`${player.statistics[0].team.name} logo`}
-                    />
-                    <p className="text-base uppercase text-slate-50">
-                      {player.statistics[0].team.name}
-                    </p>
-                  </div>
-                </li>
-              </ul>
+            <div className="flex items-center justify-between">
+              <div className="flex flex-col w-full">
+                <h2 className="font-heading text-4xl text-slate-50 uppercase mb-1">
+                  {player.player.lastname}
+                </h2>
+                <h1 className="font-heading text-6xl text-outline uppercase mb-12">
+                  {player.player.firstname}
+                </h1>
+                <div className="grid grid-cols-8">
+                  <ul className="col-span-4">
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Role
+                      </h3>
+                      <p className="text-base uppercase text-slate-50">
+                        {player.statistics[0].games.position}
+                      </p>
+                    </li>
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Country
+                      </h3>
+                      <div className="flex items-center">
+                        <img
+                          className="h-5 w-auto mr-2"
+                          src={`https://flagsapi.com/${
+                            countries[player.player.nationality]
+                          }/flat/64.png`}
+                          alt={`${player.player.nationality} flag`}
+                        />
+                        <p className="text-base uppercase text-slate-50">
+                          {player.player.nationality}
+                        </p>
+                      </div>
+                    </li>
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Team
+                      </h3>
+                      <div className="flex items-center">
+                        <img
+                          className="w-6 h-6 mr-2"
+                          src={player.statistics[0].team.logo}
+                          alt={`${player.statistics[0].team.name} logo`}
+                        />
+                        <p className="text-base uppercase text-slate-50">
+                          {player.statistics[0].team.name}
+                        </p>
+                      </div>
+                    </li>
+                  </ul>
 
-              <ul className="">
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">
-                    Weight
-                  </h3>
-                  <p className="text-base uppercase text-slate-50">
-                    {player.statistics[0].games.position}
-                  </p>
-                </li>
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">
-                    Height
-                  </h3>
-                  <p className="text-base uppercase text-slate-50">
-                    {cmToFeetAndInches(player.player.height)}
-                  </p>
-                </li>
-                <li className="mb-4">
-                  <h3 className="text-s text-slate-500 uppercase mb-2">
-                    Birth
-                  </h3>
-                  <p className="text-base uppercase text-slate-50">
-                    {player.player.birth.date} ({player.player.age} years old)
-                  </p>
-                </li>
-              </ul>
-
-              <div className="relative w-64 self-end">
+                  <ul className="col-span-4">
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Weight
+                      </h3>
+                      <p className="text-base uppercase text-slate-50">
+                        {player.statistics[0].games.position}
+                      </p>
+                    </li>
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Height
+                      </h3>
+                      <p className="text-base uppercase text-slate-50">
+                        {cmToFeetAndInches(player.player.height)}
+                      </p>
+                    </li>
+                    <li className="mb-4">
+                      <h3 className="text-s text-slate-500 uppercase mb-2">
+                        Birth
+                      </h3>
+                      <p className="text-base uppercase text-slate-50">
+                        {player.player.birth.date} ({player.player.age} years
+                        old)
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="relative w-1/3">
                 <img
-                  className="w-64 rounded-full"
+                  className="w-full rounded-full"
                   src={player.player.photo}
                   alt={`${player.player.firstname} ${player.player.lastname}`}
                 />
                 <img
-                  className="absolute right-44 top-40 w-40 h-40"
+                  className="absolute right-60 top-48 w-40 h-40"
                   src={player.statistics[0].team.logo}
                   alt={`${player.statistics[0].team.name} logo`}
                 />
