94150fc3b5395e17ba628b6f3bd27c813cba0e33
Isaac Rossi <isaacjohnrossi@gmail.com>
2024-10-24 15:31:59 +1100

    Adjusted some of the positioning within the ImageWithLogo component.

--- src/pages/PlayerDetails/components/HeaderWithDetails.jsx
+++ src/pages/PlayerDetails/components/HeaderWithDetails.jsx
@@ -53,14 +53,14 @@ const HeaderWithDetails = ({ data }) => (
 );
 
 const ImageWithLogo = ({ data }) => (
-  <div className="relative w-1/3">
+  <div className="relative w-1/3 h-fit">
     <img
       className="w-full rounded-full"
       src={data.player.photo}
       alt={`${data.player.firstname} ${data.player.lastname}`}
     />
     <img
-      className="absolute right-60 top-48 w-40 h-40"
+      className="absolute w-32 h-32 top-2/3 left-[-19%]"
       src={data.statistics[0].team.logo}
       alt={`${data.statistics[0].team.name} logo`}
     />
--- src/pages/PlayerDetails/components/ListItemWithImage.jsx
+++
@@ -1,9 +0,0 @@
-export const ListItemWithImage = ({ title, imgSrc, imgAlt, children }) => (
-  <li className="mb-4">
-    <h3 className="text-s text-slate-500 uppercase mb-2">{title}</h3>
-    <div className="flex items-center">
-      <img className="w-6 h-6 mr-2" src={imgSrc} alt={imgAlt} />
-      <p className="text-base uppercase text-slate-50">{children}</p>
-    </div>
-  </li>
-);
