f638d0e2dbbc44bfc03fdacbfc70b457198420a6
Isaac Rossi <isaacjohnrossi@gmail.com>
2025-01-15 20:09:06 +1100

    Removed unnecessary TableCell component in Table and finished responsive styles.

--- src/pages/Players/components/Table.jsx
+++ src/pages/Players/components/Table.jsx
@@ -5,42 +5,46 @@ import { headers } from "../../../data/headers";
 import { Link } from "react-router-dom";
 
 const Table = ({ list }) => (
-  <table className="w-full mt-14 text-sm lg:text-base">
+  <table className="w-full min-w-[380px] mt-14 text-sm lg:text-base">
     {list.length !== 0 && <TableHeader />}
     <TableBody list={list} />
   </table>
 );
 
 const TableRow = ({ item, isHeader = false }) => (
-  <tr className="grid grid-cols-7 justify-between p-5 text-s text-left uppercase text-slate-50 border-b border-red-600">
+  <tr className="grid grid-cols-5 md:grid-cols-7 justify-between p-2 md:p-5 text-s text-left uppercase text-slate-50 border-b border-red-600 gap-x-4">
     {isHeader ? (
       item.map((header, index) => (
         <th
           key={index}
-          className="text-slate-400 font-medium md:[&:nth-child(-n+3)]:col-span-2 first:col-span-3 [&:nth-child(2)]:col-span-2"
+          className="text-slate-400 font-medium md:[&:nth-child(-n+3)]:col-span-2 first:col-span-2 md:first:col-span-3 [&:nth-child(2)]:col-span-1 md:[&:nth-child(2)]:col-span-2 last:text-right last:md:text-left"
         >
           {header}
         </th>
       ))
     ) : (
       <>
-        <TableCell>
+        <td className="col-span-2">
           <Link to={`/details/${item.player.id}`}>{item.player.lastname}</Link>
-        </TableCell>
-        <TableCell className="flex">
+        </td>
+
+        <td className="flex md:col-span-2">
           <img
             className="w-6 h-6 mr-2"
             src={item.statistics[0].team.logo}
             alt={`${item.statistics[0].team.name} logo`}
           />
-          <p className="hidden lg:inline">
-            {shortenToTwo(item.statistics[0].team.name)}
-          </p>
-          <p className="lg:hidden">
-            {shortenToOne(item.statistics[0].team.name)}
-          </p>
-        </TableCell>
-        <TableCell className="flex ">
+          <div className="hidden md:inline">
+            <p className="hidden lg:inline">
+              {shortenToTwo(item.statistics[0].team.name)}
+            </p>
+            <p className="lg:hidden">
+              {shortenToOne(item.statistics[0].team.name)}
+            </p>
+          </div>
+        </td>
+
+        <td className="flex md:col-span-2">
           <img
             className="h-5 w-auto mr-2"
             src={`https://flagsapi.com/${
@@ -49,15 +53,16 @@ const TableRow = ({ item, isHeader = false }) => (
             alt="country flag"
           />
           <p className="hidden md:inline">{item.player.nationality}</p>
-        </TableCell>
-        <TableCell className="justify-self-end">
+        </td>
+
+        <td className="text-right md:text-left">
           <span className="hidden md:inline">
             {item.statistics[0].games.position}
           </span>
           <span className="md:hidden">
             {trimString(item.statistics[0].games.position)}
           </span>
-        </TableCell>
+        </td>
       </>
     )}
   </tr>
@@ -75,12 +80,6 @@ const TableBody = ({ list }) => (
       <TableRow key={item.player.id} item={item} />
     ))}
   </tbody>
-);
-
-const TableCell = ({ children }) => (
-  <td className="flex md:[&:nth-child(-n+3)]:col-span-2 first:col-span-3 [&:nth-child(2)]:col-span-2 font-medium">
-    {children}
-  </td>
 );
 
 export { Table, TableHeader, TableBody, TableRow };
